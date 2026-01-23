f233915ce88470ed277130ec40a8aa7525943050
Siripurapu-MohanChandu <mohanchandusiripurapu@gmail.com>
2026-01-23 09:53:36 +0530

    updated app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { Home } from './home/home';
+import { MovieGenre } from './movie-genre/movie-genre';
+export const routes: Routes = [
+    {path: '', component: Home},
+    {path: 'genre', component: MovieGenre}
+];
